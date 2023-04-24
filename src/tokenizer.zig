9c5c433f08c72e7c1f606d00c6fe666bd0fa3593
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-04-23 21:43:47 -0700

    tokenize type annotation

--- src/tokenizer.zig
+++ src/tokenizer.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const List = std.ArrayList;
+const Tuple = std.meta.Tuple;
 
 const interner = @import("interner.zig");
 const Intern = interner.Intern;
@@ -8,10 +9,15 @@ const Interned = interner.Interned;
 
 const Kind = union(enum) {
     symbol: Interned,
+    int: Interned,
+    float: Interned,
     equal,
     backslash,
     dot,
+    colon,
     plus,
+    minus,
+    arrow,
 };
 
 const Pos = struct {
@@ -20,7 +26,7 @@ const Pos = struct {
 };
 
 const Span = struct {
-    start: Pos,
+    begin: Pos,
     end: Pos,
 };
 
@@ -62,21 +68,76 @@ fn advance(cursor: *Cursor, n: usize) []const u8 {
     return value;
 }
 
+fn number(intern: *Intern, tokens: *Tokens, cursor: *Cursor) !void {
+    const begin = cursor.pos;
+    var i: usize = 1;
+    var decimals: usize = if (cursor.source[0] == '.') 1 else 0;
+    while (i < cursor.source.len) : (i += 1) {
+        switch (cursor.source[i]) {
+            '0'...'9' => {},
+            '.' => decimals += 1,
+            else => break,
+        }
+    }
+    if (i > 2 and cursor.source[i - 1] == '.') {
+        i -= 1;
+        decimals -= 1;
+    }
+    const string = cursor.source[0..i];
+    if (string.len == 1) {
+        switch (string[0]) {
+            '-' => return choice(tokens, cursor, .minus, &.{.{ '>', .arrow }}),
+            '.' => {
+                _ = advance(cursor, i);
+                try tokens.span.append(.{ .begin = begin, .end = cursor.pos });
+                try tokens.kind.append(.dot);
+                return;
+            },
+            else => {},
+        }
+    }
+    _ = advance(cursor, i);
+    try tokens.span.append(.{ .begin = begin, .end = cursor.pos });
+    const interned = try intern.string(string);
+    if (decimals == 0) return try tokens.kind.append(.{ .int = interned });
+    try tokens.kind.append(.{ .float = interned });
+}
+
 fn exact(tokens: *Tokens, cursor: *Cursor, kind: Kind) !void {
-    const start = cursor.pos;
+    const begin = cursor.pos;
     _ = advance(cursor, 1);
     try tokens.kind.append(kind);
-    try tokens.span.append(.{ .start = start, .end = cursor.pos });
+    try tokens.span.append(.{ .begin = begin, .end = cursor.pos });
+}
+
+const Choice = Tuple(&.{ u8, Kind });
+
+fn choice(tokens: *Tokens, cursor: *Cursor, kind: Kind, choices: []const Choice) !void {
+    const begin = cursor.pos;
+    if (cursor.source.len > 1) {
+        const t = cursor.source[1];
+        for (choices) |c| {
+            if (t == c[0]) {
+                _ = advance(cursor, 2);
+                try tokens.span.append(.{ .begin = begin, .end = cursor.pos });
+                try tokens.kind.append(c[1]);
+                return;
+            }
+        }
+    }
+    _ = advance(cursor, 1);
+    try tokens.span.append(.{ .begin = begin, .end = cursor.pos });
+    try tokens.kind.append(kind);
 }
 
 fn symbol(tokens: *Tokens, intern: *Intern, cursor: *Cursor) !void {
-    const start = cursor.pos;
+    const begin = cursor.pos;
     var i: usize = 0;
     while (i < cursor.source.len and !reserved(cursor.source[i])) : (i += 1) {}
     const string = advance(cursor, i);
     const interned = try intern.string(string);
     const end = cursor.pos;
-    const span = Span{ .start = start, .end = end };
+    const span = Span{ .begin = begin, .end = end };
     try tokens.kind.append(.{ .symbol = interned });
     try tokens.span.append(span);
 }
@@ -93,9 +154,10 @@ pub fn tokenize(allocator: Allocator, intern: *Intern, source: []const u8) !Toke
     while (cursor.source.len != 0) {
         trim(&cursor);
         switch (cursor.source[0]) {
+            '0'...'9', '-', '.' => try number(intern, &tokens, &cursor),
             '=' => try exact(&tokens, &cursor, .equal),
             '\\' => try exact(&tokens, &cursor, .backslash),
-            '.' => try exact(&tokens, &cursor, .dot),
+            ':' => try exact(&tokens, &cursor, .colon),
             '+' => try exact(&tokens, &cursor, .plus),
             else => try symbol(&tokens, intern, &cursor),
         }
@@ -113,10 +175,21 @@ pub fn toString(allocator: Allocator, intern: Intern, tokens: Tokens) ![]const u
                 const string = intern.lookup(interned);
                 try std.fmt.format(writer, "symbol {s}", .{string});
             },
+            .int => |interned| {
+                const string = intern.lookup(interned);
+                try std.fmt.format(writer, "int {s}", .{string});
+            },
+            .float => |interned| {
+                const string = intern.lookup(interned);
+                try std.fmt.format(writer, "float {s}", .{string});
+            },
             .equal => try writer.writeAll("equal"),
             .backslash => try writer.writeAll("backslash"),
             .dot => try writer.writeAll("dot"),
+            .colon => try writer.writeAll("colon"),
             .plus => try writer.writeAll("plus"),
+            .minus => try writer.writeAll("minus"),
+            .arrow => try writer.writeAll("arrow"),
         }
     }
     return list.toOwnedSlice();
@@ -128,19 +201,22 @@ pub fn toSource(allocator: Allocator, intern: Intern, tokens: Tokens) ![]const u
     var pos = Pos{ .line = 1, .column = 1 };
     for (tokens.kind.items) |kind, i| {
         const span = tokens.span.items[i];
-        const delta = span.start.column - pos.column;
+        const delta = span.begin.column - pos.column;
         var j: usize = 0;
         while (j < delta) : (j += 1) try writer.writeAll(" ");
         pos.column = span.end.column;
         switch (kind) {
-            .symbol => |interned| {
+            .symbol, .int, .float => |interned| {
                 const string = intern.lookup(interned);
                 try writer.writeAll(string);
             },
             .equal => try writer.writeAll("="),
             .backslash => try writer.writeAll("\\"),
             .dot => try writer.writeAll("."),
+            .colon => try writer.writeAll(":"),
             .plus => try writer.writeAll("+"),
+            .minus => try writer.writeAll("-"),
+            .arrow => try writer.writeAll("->"),
         }
     }
     return list.toOwnedSlice();
--- test/test_function.zig
+++ test/test_function.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const fusion = @import("fusion");
 
-test "tokenize" {
+test "tokenize with no annotation" {
     const allocator = std.testing.allocator;
     const source =
         \\double = \x. x + x
@@ -14,6 +14,37 @@ test "tokenize" {
     defer allocator.free(actual);
     const expected =
         \\symbol double
+        \\equal
+        \\backslash
+        \\symbol x
+        \\dot
+        \\symbol x
+        \\plus
+        \\symbol x
+    ;
+    try std.testing.expectEqualStrings(expected, actual);
+    const reconstructed = try fusion.tokenizer.toSource(allocator, intern, tokens);
+    defer allocator.free(reconstructed);
+    try std.testing.expectEqualStrings(source, reconstructed);
+}
+
+test "tokenize with annotation" {
+    const allocator = std.testing.allocator;
+    const source =
+        \\double: I32 -> I32 = \x. x + x
+    ;
+    var intern = fusion.Intern.init(allocator);
+    defer intern.deinit();
+    const tokens = try fusion.tokenizer.tokenize(allocator, &intern, source);
+    defer tokens.deinit();
+    const actual = try fusion.tokenizer.toString(allocator, intern, tokens);
+    defer allocator.free(actual);
+    const expected =
+        \\symbol double
+        \\colon
+        \\symbol I32
+        \\arrow
+        \\symbol I32
         \\equal
         \\backslash
         \\symbol x
