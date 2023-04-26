0b3b756e6dd68272ff71e6414a67695faa960180
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-04-26 08:53:18 -0700

    pretty print ast

--- src/parser.zig
+++ src/parser.zig
@@ -13,6 +13,8 @@ const Indent = tokenizer.Indent;
 const Kind = enum {
     symbol,
     define,
+    lambda,
+    binary_op,
 };
 
 const Define = struct {
@@ -25,6 +27,16 @@ const Lambda = struct {
     body: Expression,
 };
 
+const BinaryOpKind = enum {
+    add,
+};
+
+const BinaryOp = struct {
+    kind: BinaryOpKind,
+    left: Expression,
+    right: Expression,
+};
+
 pub const Ast = struct {
     kind: List(Kind),
     span: List(Span),
@@ -32,6 +44,7 @@ pub const Ast = struct {
     symbol: List(Interned),
     define: List(Define),
     lambda: List(Lambda),
+    binary_op: List(BinaryOp),
     top_level: List(Expression),
 
     pub fn deinit(self: Ast) void {
@@ -40,6 +53,8 @@ pub const Ast = struct {
         self.index.deinit();
         self.symbol.deinit();
         self.define.deinit();
+        self.lambda.deinit();
+        self.binary_op.deinit();
         self.top_level.deinit();
     }
 };
@@ -48,14 +63,15 @@ const Precedence = u32;
 
 const LOWEST = 0;
 const DEFINE = LOWEST + 10;
-const HIGHEST = DEFINE + 10;
+const ADD = DEFINE + 10;
+const HIGHEST = ADD + 10;
 
 const Expression = u64;
 
 const Context = struct {
     allocator: Allocator,
     tokens: Tokens,
-    ast: Ast,
+    ast: *Ast,
     token_index: u64,
     precedence: Precedence,
     indent: Indent,
@@ -86,7 +102,7 @@ fn lambda(context: *Context) !Expression {
     const body = try expression(context);
     const self = context.ast.kind.items.len;
     const end = context.ast.span.items[body].end;
-    try context.ast.kind.append(.symbol);
+    try context.ast.kind.append(.lambda);
     try context.ast.span.append(.{ .begin = begin, .end = end });
     try context.ast.index.append(context.ast.lambda.items.len);
     try context.ast.lambda.append(.{ .parameters = params, .body = body });
@@ -121,23 +137,41 @@ fn define(context: *Context, name: Expression) !Expression {
     return self;
 }
 
+fn binaryOp(context: *Context, left: Expression, kind: BinaryOpKind) !Expression {
+    context.token_index += 1;
+    const right = try expression(context);
+    const span = Span{
+        .begin = context.ast.span.items[left].begin,
+        .end = context.ast.span.items[right].end,
+    };
+    const self = context.ast.kind.items.len;
+    try context.ast.kind.append(.binary_op);
+    try context.ast.span.append(span);
+    try context.ast.index.append(context.ast.binary_op.items.len);
+    try context.ast.binary_op.append(.{ .kind = kind, .left = left, .right = right });
+    return self;
+}
+
 const Infix = struct {
     precedence: Precedence,
     asscociativity: Asscociativity,
-    kind: enum {
+    kind: union(enum) {
         define,
+        binary_op: BinaryOpKind,
     },
 
     fn parse(self: Infix, context: *Context, left: Expression) !Expression {
         switch (self.kind) {
             .define => return try define(context, left),
+            .binary_op => |kind| return try binaryOp(context, left, kind),
         }
     }
 };
 
 fn infix(context: *Context) ?Infix {
     switch (context.tokens.kind.items[context.token_index]) {
-        .equal => return .{ .kind = .define, .precedence = 1, .asscociativity = .right },
+        .equal => return .{ .kind = .define, .precedence = DEFINE, .asscociativity = .right },
+        .plus => return .{ .kind = .{ .binary_op = .add }, .precedence = ADD, .asscociativity = .left },
         else => return null,
     }
 }
@@ -163,12 +197,13 @@ pub fn parse(allocator: Allocator, tokens: Tokens) !Ast {
         .symbol = List(Interned).init(allocator),
         .define = List(Define).init(allocator),
         .lambda = List(Lambda).init(allocator),
+        .binary_op = List(BinaryOp).init(allocator),
         .top_level = List(Expression).init(allocator),
     };
     var context = Context{
         .allocator = allocator,
         .tokens = tokens,
-        .ast = ast,
+        .ast = &ast,
         .token_index = 0,
         .precedence = LOWEST,
         .indent = Indent{ .space = 0 },
@@ -178,6 +213,44 @@ pub fn parse(allocator: Allocator, tokens: Tokens) !Ast {
     return ast;
 }
 
-pub fn toString(_: Allocator, _: Intern, _: Ast) ![]u8 {
-    return "";
+fn symbolToString(writer: List(u8).Writer, intern: Intern, ast: Ast, expr: Expression) !void {
+    const s = ast.symbol.items[ast.index.items[expr]];
+    try writer.writeAll(intern.lookup(s));
+}
+
+fn defineToString(writer: List(u8).Writer, intern: Intern, ast: Ast, expr: Expression) !void {
+    const d = ast.define.items[ast.index.items[expr]];
+    try writer.writeAll("(def ");
+    try symbolToString(writer, intern, ast, d.name);
+    try writer.writeAll(" ");
+    try expressionToString(writer, intern, ast, d.value);
+    try writer.writeAll(")");
+}
+
+fn lambdaToString(writer: List(u8).Writer, intern: Intern, ast: Ast, expr: Expression) !void {
+    const l = ast.lambda.items[ast.index.items[expr]];
+    try writer.writeAll("(fn (");
+    for (l.parameters.items) |p, i| {
+        try expressionToString(writer, intern, ast, p);
+        if (i < l.parameters.items.len - 1) try writer.writeAll(" ");
+    }
+    try writer.writeAll(") ");
+    try expressionToString(writer, intern, ast, l.body);
+    try writer.writeAll(")");
+}
+
+fn expressionToString(writer: List(u8).Writer, intern: Intern, ast: Ast, expr: Expression) error{OutOfMemory}!void {
+    switch (ast.kind.items[expr]) {
+        .symbol => try symbolToString(writer, intern, ast, expr),
+        .define => try defineToString(writer, intern, ast, expr),
+        .lambda => try lambdaToString(writer, intern, ast, expr),
+        else => std.debug.panic("Unknown kind {}\n", .{ast.kind.items[expr]}),
+    }
+}
+
+pub fn toString(allocator: Allocator, intern: Intern, ast: Ast) ![]u8 {
+    var list = List(u8).init(allocator);
+    const writer = list.writer();
+    for (ast.top_level.items) |expr| try expressionToString(writer, intern, ast, expr);
+    return list.toOwnedSlice();
 }
