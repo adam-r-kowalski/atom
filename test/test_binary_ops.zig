ba32bbf2f5cd147dfac5707d76e71afb70839438
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-05-18 19:55:15 -0700

    type infer binary op with new syntax

--- test/test_binary_ops.zig
+++ test/test_binary_ops.zig
@@ -79,137 +79,116 @@ test "parse grouped greater" {
     try std.testing.expectEqualStrings(expected, actual);
 }
 
-test "type result of add has same type as operands" {
+test "type infer binary op add" {
     const allocator = std.testing.allocator;
-    const source = "add(x: i32, y: i32) = x + y";
+    const source = "fn add(x: i32, y: i32) -> i32 = x + y";
     const actual = try atom.testing.typeInfer(allocator, source, "add");
     defer allocator.free(actual);
-    const expected = "add(x: i32, y: i32) -> i32 = x + y";
+    const expected =
+        \\function
+        \\    name = add
+        \\    parameters =
+        \\        parameter =
+        \\            symbol =
+        \\                name = x
+        \\                type = i32
+        \\        parameter =
+        \\            symbol =
+        \\                name = y
+        \\                type = i32
+        \\    return_type = i32
+        \\    body =
+        \\        binary_op =
+        \\            kind = +
+        \\            left =
+        \\                symbol =
+        \\                    name = x
+        \\                    type = i32
+        \\            right =
+        \\                symbol =
+        \\                    name = y
+        \\                    type = i32
+        \\            type = i32
+    ;
     try std.testing.expectEqualStrings(expected, actual);
 }
 
-test "type operands should have the same type as the result" {
+test "type infer binary op multiply" {
     const allocator = std.testing.allocator;
-    const source = "add(x, y) -> i32 = x + y";
-    const actual = try atom.testing.typeInfer(allocator, source, "add");
+    const source = "fn multiply(x: i32, y: i32) -> i32 = x * y";
+    const actual = try atom.testing.typeInfer(allocator, source, "multiply");
     defer allocator.free(actual);
-    const expected = "add(x: i32, y: i32) -> i32 = x + y";
+    const expected =
+        \\function
+        \\    name = multiply
+        \\    parameters =
+        \\        parameter =
+        \\            symbol =
+        \\                name = x
+        \\                type = i32
+        \\        parameter =
+        \\            symbol =
+        \\                name = y
+        \\                type = i32
+        \\    return_type = i32
+        \\    body =
+        \\        binary_op =
+        \\            kind = *
+        \\            left =
+        \\                symbol =
+        \\                    name = x
+        \\                    type = i32
+        \\            right =
+        \\                symbol =
+        \\                    name = y
+        \\                    type = i32
+        \\            type = i32
+    ;
     try std.testing.expectEqualStrings(expected, actual);
 }
 
-test "type infer can figure out second parameter and return type" {
+test "type infer binary op multiply then add" {
     const allocator = std.testing.allocator;
-    const source = "add(x: i32, y) = x + y";
-    const actual = try atom.testing.typeInfer(allocator, source, "add");
-    defer allocator.free(actual);
-    const expected = "add(x: i32, y: i32) -> i32 = x + y";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer can figure out first parameter and return type" {
-    const allocator = std.testing.allocator;
-    const source = "add(x, y: i32) = x + y";
-    const actual = try atom.testing.typeInfer(allocator, source, "add");
-    defer allocator.free(actual);
-    const expected = "add(x: i32, y: i32) -> i32 = x + y";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer fully generic add" {
-    const allocator = std.testing.allocator;
-    const source = "add(x, y) = x + y";
-    const actual = try atom.testing.typeInfer(allocator, source, "add");
-    defer allocator.free(actual);
-    const expected = "add[A](x: A, y: A) -> A = x + y";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer multiply" {
-    const allocator = std.testing.allocator;
-    const source = "mul(x: i32, y: i32) = x * y";
-    const actual = try atom.testing.typeInfer(allocator, source, "mul");
-    defer allocator.free(actual);
-    const expected = "mul(x: i32, y: i32) -> i32 = x * y";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer multiply then add infer last two parameter types and return type" {
-    const allocator = std.testing.allocator;
-    const source = "f(x: i32, y, z) = x * y + z";
+    const source = "fn f(x: i32, y: i32, z: i32) -> i32 = x * y + z";
     const actual = try atom.testing.typeInfer(allocator, source, "f");
     defer allocator.free(actual);
-    const expected = "f(x: i32, y: i32, z: i32) -> i32 = x * y + z";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer multiply then add infer first and last parameter types and return type" {
-    const allocator = std.testing.allocator;
-    const source = "f(x, y: i32, z) = x * y + z";
-    const actual = try atom.testing.typeInfer(allocator, source, "f");
-    defer allocator.free(actual);
-    const expected = "f(x: i32, y: i32, z: i32) -> i32 = x * y + z";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer multiply then add infer first two parameter types and return type" {
-    const allocator = std.testing.allocator;
-    const source = "f(x, y, z: i32) = x * y + z";
-    const actual = try atom.testing.typeInfer(allocator, source, "f");
-    defer allocator.free(actual);
-    const expected = "f(x: i32, y: i32, z: i32) -> i32 = x * y + z";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer multiply then add infer all parameter types" {
-    const allocator = std.testing.allocator;
-    const source = "f(x, y, z) -> i32 = x * y + z";
-    const actual = try atom.testing.typeInfer(allocator, source, "f");
-    defer allocator.free(actual);
-    const expected = "f(x: i32, y: i32, z: i32) -> i32 = x * y + z";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer add then multiply infer last two parameter types and return type" {
-    const allocator = std.testing.allocator;
-    const source = "f(x: i32, y, z) = x + y * z";
-    const actual = try atom.testing.typeInfer(allocator, source, "f");
-    defer allocator.free(actual);
-    const expected = "f(x: i32, y: i32, z: i32) -> i32 = x + y * z";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer add then multiply infer first and last parameter types and return type" {
-    const allocator = std.testing.allocator;
-    const source = "f(x, y: i32, z) = x + y * z";
-    const actual = try atom.testing.typeInfer(allocator, source, "f");
-    defer allocator.free(actual);
-    const expected = "f(x: i32, y: i32, z: i32) -> i32 = x + y * z";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer add then multiply infer first two parameter types and return type" {
-    const allocator = std.testing.allocator;
-    const source = "f(x, y, z: i32) = x + y * z";
-    const actual = try atom.testing.typeInfer(allocator, source, "f");
-    defer allocator.free(actual);
-    const expected = "f(x: i32, y: i32, z: i32) -> i32 = x + y * z";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer add then multiply infer all parameter types" {
-    const allocator = std.testing.allocator;
-    const source = "f(x, y, z) -> i32 = x + y * z";
-    const actual = try atom.testing.typeInfer(allocator, source, "f");
-    defer allocator.free(actual);
-    const expected = "f(x: i32, y: i32, z: i32) -> i32 = x + y * z";
-    try std.testing.expectEqualStrings(expected, actual);
-}
-
-test "type infer fully generic add then multiply" {
-    const allocator = std.testing.allocator;
-    const source = "f(x, y, z) = x + y * z";
-    const actual = try atom.testing.typeInfer(allocator, source, "f");
-    defer allocator.free(actual);
-    const expected = "f[A](x: A, y: A, z: A) -> A = x + y * z";
+    const expected =
+        \\function
+        \\    name = f
+        \\    parameters =
+        \\        parameter =
+        \\            symbol =
+        \\                name = x
+        \\                type = i32
+        \\        parameter =
+        \\            symbol =
+        \\                name = y
+        \\                type = i32
+        \\        parameter =
+        \\            symbol =
+        \\                name = z
+        \\                type = i32
+        \\    return_type = i32
+        \\    body =
+        \\        binary_op =
+        \\            kind = +
+        \\            left =
+        \\                binary_op =
+        \\                    kind = *
+        \\                    left =
+        \\                        symbol =
+        \\                            name = x
+        \\                            type = i32
+        \\                    right =
+        \\                        symbol =
+        \\                            name = y
+        \\                            type = i32
+        \\                    type = i32
+        \\            right =
+        \\                symbol =
+        \\                    name = z
+        \\                    type = i32
+        \\            type = i32
+    ;
     try std.testing.expectEqualStrings(expected, actual);
 }
