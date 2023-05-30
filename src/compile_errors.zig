d5cd839db2d406655638b14a6cf6b84e009e10a8
Adam Kowalski <adam.kowalski.work@gmail.com>
2023-05-29 23:36:34 -0700

    type inference error messages

--- src/compile_errors.zig
+++ src/compile_errors.zig
@@ -4,16 +4,31 @@ const Allocator = std.mem.Allocator;
 
 const Interned = @import("interner.zig").Interned;
 const Span = @import("span.zig").Span;
+const substitution = @import("substitution.zig");
+const Monotype = substitution.Monotype;
+const TypedSpan = substitution.TypedSpan;
+const colors = @import("colors.zig");
+const RED = colors.RED;
+const CLEAR = colors.CLEAR;
 
 fn writeSource(lines: [][]const u8, span: Span, writer: anytype) !void {
-    const index = span.begin.line - 1;
+    var index = span.begin.line - 1;
     if (index > 0) try writer.print("{} | {s}\n", .{ span.begin.line - 1, lines[index - 1] });
     const line = lines[index];
     try writer.print("{} | {s}", .{ span.begin.line, line[0 .. span.begin.column - 1] });
-    try writer.writeAll("\x1b[31m");
-    try writer.writeAll(line[span.begin.column - 1 .. span.end.column - 1]);
-    try writer.writeAll("\x1b[0m");
-    try writer.writeAll(line[span.end.column - 1 ..]);
+    try writer.writeAll(RED);
+    if (span.begin.line == span.end.line) {
+        try writer.writeAll(line[span.begin.column - 1 .. span.end.column - 1]);
+        try writer.writeAll(CLEAR);
+        try writer.writeAll(line[span.end.column - 1 ..]);
+    } else {
+        try writer.writeAll(line[span.begin.column - 1 ..]);
+        for (span.begin.line..span.end.line) |i| {
+            try writer.print("\n{} | {s}", .{ i + 1, lines[i] });
+        }
+        try writer.writeAll(CLEAR);
+    }
+    index = span.end.line - 1;
     if (index < lines.len - 1) try writer.print("\n{} | {s}", .{ span.begin.line + 1, lines[index + 1] });
 }
 
@@ -45,12 +60,49 @@ pub const UndefinedVariable = struct {
     }
 };
 
+pub const TypeError = struct {
+    left: TypedSpan,
+    right: TypedSpan,
+
+    fn toString(self: TypeError, lines: [][]const u8, writer: anytype) !void {
+        try writer.print(
+            \\--- TYPE ERROR ---------------------------------------------------
+            \\
+            \\Here the inferred type is {}
+            \\
+            \\
+        , .{self.left.type});
+        if (self.left.span) |span| {
+            try writeSource(lines, span, writer);
+        }
+        try writer.print(
+            \\
+            \\
+            \\Here the inferred type is {}
+            \\
+            \\
+        , .{self.right.type});
+        if (self.right.span) |span| {
+            try writeSource(lines, span, writer);
+        }
+        try writer.print(
+            \\
+            \\
+            \\Expected these two types to be the same.
+            \\
+            \\
+        , .{});
+    }
+};
+
 pub const CompileError = union(enum) {
     undefined_variable: UndefinedVariable,
+    type_error: TypeError,
 
     fn toString(self: CompileError, lines: [][]const u8, writer: anytype) !void {
         switch (self) {
             .undefined_variable => try self.undefined_variable.toString(lines, writer),
+            .type_error => try self.type_error.toString(lines, writer),
         }
     }
 };
--- test/test_compile_errors.zig
+++ test/test_compile_errors.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 const neuron = @import("neuron");
+const RED = neuron.colors.RED;
+const CLEAR = neuron.colors.CLEAR;
 
 test "use of undefined variable" {
     const allocator = std.testing.allocator;
@@ -23,38 +25,76 @@ test "use of undefined variable" {
         \\
         \\    start
         \\
-    , .{ "\x1b[31m", "\x1b[0m" });
+    , .{ RED, CLEAR });
     defer allocator.free(expected);
     try std.testing.expectEqualStrings(expected, actual);
 }
 
-// test "type error" {
-//     const allocator = std.testing.allocator;
-//     const source =
-//         \\start = fn(x: i32, y: f64) f32 {
-//         \\    if x == y {
-//         \\        x
-//         \\    } else {
-//         \\        y
-//         \\    }
-//         \\}
-//     ;
-//     const actual = try neuron.testing.compileErrors(allocator, source);
-//     defer allocator.free(actual);
-//     const expected = try std.fmt.allocPrint(allocator,
-//         \\--- UNDEFINED VARIABLE ---------------------------------------------------
-//         \\
-//         \\Cannot find variable `fib`.
-//         \\
-//         \\1 | start = fn() f32 {{
-//         \\2 |     {s}fib{s}(5)
-//         \\3 | }}
-//         \\
-//         \\Maybe you want one of the following?
-//         \\
-//         \\    start
-//         \\
-//     , .{ "\x1b[31m", "\x1b[0m" });
-//     defer allocator.free(expected);
-//     try std.testing.expectEqualStrings(expected, actual);
-// }
+test "type error" {
+    const allocator = std.testing.allocator;
+    const source =
+        \\start = fn(x: i32, y: f64) f32 {
+        \\    if x == y {
+        \\        x
+        \\    } else {
+        \\        y
+        \\    }
+        \\}
+    ;
+    const actual = try neuron.testing.compileErrors(allocator, source);
+    defer allocator.free(actual);
+    const expected = try std.fmt.allocPrint(allocator,
+        \\--- TYPE ERROR ---------------------------------------------------
+        \\
+        \\Here the inferred type is i32
+        \\
+        \\1 | start = fn(x: i32, y: f64) f32 {{
+        \\2 |     if {s}x{s} == y {{
+        \\3 |         x
+        \\
+        \\Here the inferred type is f64
+        \\
+        \\1 | start = fn(x: i32, y: f64) f32 {{
+        \\2 |     if x == {s}y{s} {{
+        \\3 |         x
+        \\
+        \\Expected these two types to be the same.
+        \\
+        \\
+    , .{ RED, CLEAR, RED, CLEAR });
+    defer allocator.free(expected);
+    try std.testing.expectEqualStrings(expected, actual);
+}
+
+test "type error of define" {
+    const allocator = std.testing.allocator;
+    const source =
+        \\start = fn() f32 {
+        \\    x: f64 = 5
+        \\    x
+        \\}
+    ;
+    const actual = try neuron.testing.compileErrors(allocator, source);
+    defer allocator.free(actual);
+    const expected = try std.fmt.allocPrint(allocator,
+        \\--- TYPE ERROR ---------------------------------------------------
+        \\
+        \\Here the inferred type is f32
+        \\
+        \\1 | start = fn() {s}f32{s} {{
+        \\2 |     x: f64 = 5
+        \\
+        \\Here the inferred type is f64
+        \\
+        \\1 | start = fn() f32 {s}{{
+        \\2 |     x: f64 = 5
+        \\3 |     x
+        \\4 | }}{s}
+        \\
+        \\Expected these two types to be the same.
+        \\
+        \\
+    , .{ RED, CLEAR, RED, CLEAR });
+    defer allocator.free(expected);
+    try std.testing.expectEqualStrings(expected, actual);
+}
